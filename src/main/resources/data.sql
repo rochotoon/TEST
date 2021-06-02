c7133c88d875335bbe202503768eeb3fc3b98fe3
rochotoon <defton_7@hotmail.com>
2021-06-02 05:19:40 +0000

    qaz

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,6 +7,7 @@
 -- *
 INSERT INTO employees(EMPLOYEE_ID,first_name,middle_Initial,last_Name,Date_Of_Birth,Date_Of_Employment,STATUS) VALUES 
 (1,'JOE' ,'M','DOE',sysdate-1,sysdate,'INACTIVE'),
-(2,'JOE','F','TIGGER',sysdate-2,sysdate,'INACTIVE'),
+(2,'JOE','F','TIGGER',sysdate-2,sysdate,'ACTIVE'),
 (3,'JOSE','R','FLORES',sysdate-3,sysdate,'ACTIVE'),
-(4,'FABY','G','GARCIA',sysdate-4,sysdate,'ACTIVE');
+(4,'EMMA','ELI','CEDILLO',sysdate-4,sysdate,'ACTIVE'),
+(4,'DUDE','EL','SANTANA',sysdate-4,sysdate,'ACTIVE');
