c61c8ae978b584cd539f78b72b6fa592941ca185
rochotoon <defton_7@hotmail.com>
2021-06-02 05:30:34 +0000

    test:qw

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,4 +10,4 @@ INSERT INTO employees(EMPLOYEE_ID,first_name,middle_Initial,last_Name,Date_Of_Bi
 (2,'JOE','F','TIGGER',sysdate-2,sysdate,'ACTIVE'),
 (3,'JOSE','R','FLORES',sysdate-3,sysdate,'ACTIVE'),
 (4,'EMMA','ELI','CEDILLO',sysdate-4,sysdate,'ACTIVE'),
-(4,'DUDE','EL','SANTANA',sysdate-4,sysdate,'ACTIVE');
+(5,'DUDE','EL','SANTANA',sysdate-4,sysdate,'ACTIVE');
